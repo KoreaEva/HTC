4542b242b3b03f8d47e7af7dc583879a0ef0dda8
KoreaEva <koreaeva@outlook.com>
2026-04-08 16:32:15 +0900

    Refactor button enabling logic and update feedback messages in training portal and lab content templates

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # requirements.txt에서 모든 의존성 설치
-/opt/homebrew/opt/python@3.11/bin/python3.11 -m pip install -r requirements.txt
+python -m pip install -r requirements.txt
 
 # FastAPI 애플리케이션 실행
-/opt/homebrew/opt/python@3.11/bin/python3.11 -m uvicorn main:app --host=0.0.0.0 --port=8000 --reload
+python -m uvicorn main:app --host=0.0.0.0 --port=8000 --reload
